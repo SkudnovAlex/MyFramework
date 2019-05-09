f5b3eae66d8c00792e02448f620a8f686202910c
Skudnov Aleksandr <aleksandr.skudnov@simbersoft.com>
2019-05-09 07:34:26 +0300

    кастомизация шаблона. бренды.

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,8 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = \R::find('brand', 'LIMIT 3');
         $this->setMeta('Главная страница', 'Описание', 'Ключевые слова');
+        $this->set(compact('brands'));
     }
 }
