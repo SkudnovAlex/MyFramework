6c72fd09b8cb39c163f3117e076b35410963c505
Skudnov Aleksandr <aleksandr.skudnov@simbersoft.com>
2019-05-10 08:24:15 +0300

    блок хиты

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,9 +8,13 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        //получение брендов
         $brands = \R::find('brand', 'LIMIT 3');
+        //получение хитов
         $hits = \R::find('product', "hit = '1' AND status = '1' LIMIT 8");
+        //установка метаданных
         $this->setMeta('Главная страница', 'Описание', 'Ключевые слова');
+        //передача данных в шаблон(в вид)
         $this->set(compact('brands', 'hits'));
     }
 }
