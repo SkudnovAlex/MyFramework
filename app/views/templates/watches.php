aa05ed008f0ed27130374df69211fcc0e2434137
Skudnov Aleksandr <aleksandr.skudnov@simbersoft.com>
2019-05-17 23:01:29 +0300

    карточка товара

--- app/views/templates/watches.php
+++ app/views/templates/watches.php
@@ -175,6 +175,29 @@
 
     });
 </script>
+<script src="js/jquery.easydropdown.js"></script>
+<script type="text/javascript">
+    $(function() {
+
+        var menu_ul = $('.menu_drop > li > ul'),
+            menu_a  = $('.menu_drop > li > a');
+
+        menu_ul.hide();
+
+        menu_a.click(function(e) {
+            e.preventDefault();
+            if(!$(this).hasClass('active')) {
+                menu_a.removeClass('active');
+                menu_ul.filter(':visible').slideUp('normal');
+                $(this).addClass('active').next().stop(true,true).slideDown('normal');
+            } else {
+                $(this).removeClass('active');
+                $(this).next().stop(true,true).slideUp('normal');
+            }
+        });
+
+    });
+</script>
 <script src="megamenu/js/megamenu.js"></script>
 <script src="js/imagezoom.js"></script>
 <script defer src="js/jquery.flexslider.js"></script>
